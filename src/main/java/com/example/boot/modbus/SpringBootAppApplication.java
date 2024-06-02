85d108e4049e4c1a3a91ba228f9687dd93c4efa9
kotedo <tray202_99@mail.ru>
2024-06-02 22:28:39 +0300

    Создание основных классов и выстраивание взаимодействия, требуется дальнейшая доработка

--- src/main/java/com/example/boot/modbus/SpringBootAppApplication.java
+++ src/main/java/com/example/boot/modbus/SpringBootAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.boot.SpringBootApp;
+package com.example.boot.modbus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
